80225192e5fd57f7db2685be32ca3f9085c158c0
Jacob Hudson <jacobth@udel.edu>
2025-04-01 19:36:56 -0400

    made multiple pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import './App.css';
+//import './App.css';
 import { Button, Col, Form, Row } from 'react-bootstrap';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { Detailed } from './Pages/Detailed';
+import { Rater } from './Pages/Rater';
+import { Simple } from './Pages/Simple';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -25,58 +30,18 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="Home-header">
-        <h1>
-          The Career Helpi
-        </h1>
-      </header>
-      <div className='Questionnaire-Selection'>
-        <Row>
-          <Col className='Simple-Questionnaire'>
-            <h1 className='Simple-Header'>
-              Simple Questionnaire
-            </h1>
-            <div className='Simple-Text'>
-              This is a simple questionnaire that will ask you a few questions and give you a response based on your answers.
-            </div>
-            <Button className='Simple-Button'>
-              Start Simple Questionnaire
-            </Button>
-          </Col>
-          <Col className='Detailed-Questionnaire'>
-            <h1 className='Detailed-Header'>
-              Detailed Questionnaire
-            </h1>
-            <div className='Detailed-Text'>
-              This is a detailed questionnaire that will ask you a few questions and give you a response based on your answers.
-            </div>
-            <Button className='Detailed-Button'>
-              Start Detailed Questionnaire
-            </Button>
-          </Col>
-        </Row>
-        <Col className='Question-Rater'>
-          <h1 className='Rater-Header'>
-            Question Rater
-          </h1>
-          <div className='Rater-Text'>
-            This is a question rater that will allow you to rate our questions and improve our quiz.
-          </div>
-          <Button className='Rater-Button'>
-            Start Question Rater
-          </Button>
-        </Col>
-      </div>
+      <Router><Routes>
+      <Route path='/' element={<Home/>}/>
+        <Route path='/Detailed' element={<Detailed/>}/>
+        <Route path='/Simple' element={<Simple/>}/>
+        <Route path='/Rater' element={<Rater/>}/>
+        </Routes></Router>
       <Form className='API-Key-Form'>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-        <div>Andrew Shaffer</div>
       </Form>
-      <div>Jacob Hudson</div>
-      <b>
-        <Button>New Button</Button></b>
     </div>
   );
 }
---
+++ src/Pages/Detailed.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Detailed.css'
+
+export function Detailed(){
+    return <div className='Detailed'>Welcome to Detailed Questions!<div><Link to='/'><Button>Back To Home</Button></Link></div></div>
+}
