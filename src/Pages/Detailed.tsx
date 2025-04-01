6d737ac49b0451525390f7bb068039ff217bc3a0
Jacob Hudson <jacobth@udel.edu>
2025-04-01 19:45:34 -0400

    fixed linting issues

--- src/Pages/Detailed.tsx
+++ src/Pages/Detailed.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+//import React, { useState } from 'react';
+import { Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Detailed.css'
 
