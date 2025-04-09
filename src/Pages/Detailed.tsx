5e485e3a96475a70b2ab888ef2e58fe4dda4c5f9
dshaffer <dshaffer@udel.edu>
2025-04-09 15:57:34 -0400

    Working on getting the questions list into an array

--- src/Pages/Detailed.tsx
+++ src/Pages/Detailed.tsx
@@ -2,12 +2,63 @@
 import { Button, ButtonGroup, Col, Row} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Detailed.css'
+import './Questions.txt'
 
 export function Detailed(){
+
+    let questionList: string[] = [];
+    fetch('./Questions.txt')
+        .then(response => response.text())
+        .then(data => {
+            questionList = data.split('\n'); // Split the file content into an array
+        })
+        
+    let questionNumbers: number[] = [] //array of question numbers to be generated
+
+
+    function pickQuestion(): string{
+        let question = Math.floor(Math.random() * 30) + 1 //random number between 1-10
+        if (questionNumbers.includes(question)){ //if the question is already in the array, pick a new one
+            return pickQuestion()
+        }
+        questionNumbers.push(question) //add the question number to the array
+        return questionList[question-1] //return the question at the index of the random number
+    }
+
+
+
+    let questions: JSX.Element[] = [] //array of questions to be generated
+    let length = 10 //length of the questionnaire
+    function generateQuestions(){
+        for (let i = 0; i < length; i++){
+            questions.push(<Row className='Question' key={i}>
+                <h2 className='Question'>Question {i+1}</h2>
+                <h5 className='Question'>{pickQuestion()}</h5>
+                <Row>
+                    <ButtonGroup className='Button-Group'>
+                        <Button className='Answer'>1</Button>
+                        <Button className='Answer'>2</Button>
+                        <Button className='Answer'>3</Button>
+                        <Button className='Answer'>4</Button>
+                        <Button className='Answer'>5</Button>
+                        <Button className='Answer'>6</Button>
+                        <Button className='Answer'>7</Button>
+                        <Button className='Answer'>8</Button>
+                        <Button className='Answer'>9</Button>
+                        <Button className='Answer'>10</Button>
+                    </ButtonGroup>
+                </Row>
+            </Row>)
+        }
+    }
+
+
 
     function Submitted(){
         alert("You have submitted the questionnaire. Thank you!");
     }
+
+    generateQuestions() //call the function to generate the questions
 
     return <div className='Detailed'>
         <div className='Detailed-top'>
@@ -18,23 +69,14 @@ export function Detailed(){
         
         <div className='Detailed-Header'>
             <h1 className='Header'>Welcome to the Detailed Questionnaire</h1>
-            <p> Please answer the following questions and press Submit when you are complete to see your results.</p>
+            <p> Please answer the following questions on a scale from 1-10 with 1 being the lowest and 10 being the highest. Press Submit when you are complete to see your results.</p>
         </div>
         </div>
 
         <div className='Questionnaire'>
         <div className='Detailed-questionnaire'>
             <Col className='Questions'>
-                <Row className='Question-1'>
-                    <h2 className='Question'>1. How Gay Are You?</h2>
-                    <Row>
-                    <ButtonGroup className="Answers">
-                        <Button variant='secondary'>One</Button>
-                        <Button variant='secondary'>Two</Button>
-                        <Button variant='secondary'>Three</Button>
-                    </ButtonGroup>
-                    </Row>
-                </Row>
+                {questions}
             </Col>
         </div>
         </div>
