b20eab54f45b769873140299b5a0a2bc6975e11b
LiamGreenhalgh <Williamg@udel.edu>
2025-04-09 15:28:48 -0400

    added simple question labels

--- src/Pages/Simple.tsx
+++ src/Pages/Simple.tsx
@@ -3,5 +3,61 @@ import { Button } from 'react-bootstrap';
 import './Simple.css'
 import { Link } from 'react-router-dom';
 export function Simple(){
-    return <div className='Simple'>Welcome to Simple Questions!<div><Link to='/'><Button>Back To Home</Button></Link></div></div>
+    return (
+    <div className='Simple'>
+        Welcome to Simple Questions!
+        <div>
+            <Link to='/'>
+                <Button>Back To Home</Button>
+            </Link>
+        </div>
+            <div className='Simple'>Question 0</div>
+            <div className='Simple'>Question 1</div>
+            <div className='Simple'>Question 2</div>
+            <div className='Simple'>Question 3</div>
+            <div className='Simple'>Question 4</div>
+            <div className='Simple'>Question 5</div>
+            <div className='Simple'>Question 6</div>
+            <div className='Simple'>Question 7</div>
+            <div className='Simple'>Question 8</div>
+            <div className='Simple'>Question 9</div>
+            <div className='Simple'>Question 10</div>
+            <div className='Simple'>Question 11</div>
+            <div className='Simple'>Question 12</div>
+            <div className='Simple'>Question 13</div>
+            <div className='Simple'>Question 14</div>
+            <div className='Simple'>Question 15</div>
+            <div className='Simple'>Question 16</div>
+            <div className='Simple'>Question 17</div>
+            <div className='Simple'>Question 18</div>
+            <div className='Simple'>Question 19</div>
+            <div className='Simple'>Question 20</div>
+            <div className='Simple'>Question 21</div>
+            <div className='Simple'>Question 22</div>
+            <div className='Simple'>Question 23</div>
+            <div className='Simple'>Question 24</div>
+            <div className='Simple'>Question 25</div>
+            <div className='Simple'>Question 26</div>
+            <div className='Simple'>Question 27</div>
+            <div className='Simple'>Question 28</div>
+            <div className='Simple'>Question 29</div>
+            <div className='Simple'>Question 30</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    );
 }
