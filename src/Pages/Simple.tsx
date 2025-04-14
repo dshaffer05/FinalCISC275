600d4030545e261ea656fad61bcacc1dd234437b
LiamGreenhalgh <Williamg@udel.edu>
2025-04-14 15:29:42 -0400

    Added simple questions

--- src/Pages/Simple.tsx
+++ src/Pages/Simple.tsx
@@ -12,51 +12,67 @@ export function Simple(){
             </Link>
         </div>
             <div className='Simple'>Question 0</div>
+            <div className='Simple'>What subjects do you enjoy the most in school?</div>
             <div className='Simple'>Question 1</div>
+            <div className='Simple'>What hobbies or activities do you enjoy doing in your free time?</div>
             <div className='Simple'>Question 2</div>
+            <div className='Simple'>Would you rather work with people, data, tools, or ideas?</div>
             <div className='Simple'>Question 3</div>
+            <div className='Simple'>Do you enjoy solving problems or creating new things?</div>
             <div className='Simple'>Question 4</div>
+            <div className='Simple'>Would you prefer working indoors or outdoors?</div>
             <div className='Simple'>Question 5</div>
+            <div className='Simple'>What are you naturally good at (e.g., writing, math, drawing, organizing)?</div>
             <div className='Simple'>Question 6</div>
+            <div className='Simple'>Do you enjoy working with your hands or using technology?</div>
             <div className='Simple'>Question 7</div>
+            <div className='Simple'>Are you more comfortable working alone or in a team?</div>
             <div className='Simple'>Question 8</div>
+            <div className='Simple'>Do you like planning things out or going with the flow?</div>
             <div className='Simple'>Question 9</div>
+            <div className='Simple'>Do you enjoy leading projects or supporting others?</div>
             <div className='Simple'>Question 10</div>
+            <div className='Simple'>Is it important for your future job to make a lot of money?</div>
             <div className='Simple'>Question 11</div>
+            <div className='Simple'>Do you want a career where you help people?</div>
             <div className='Simple'>Question 12</div>
+            <div className='Simple'>Do you want a job that lets you be creative?</div>
             <div className='Simple'>Question 13</div>
+            <div className='Simple'>Do you want a job that changes often or stays consistent?</div>
             <div className='Simple'>Question 14</div>
+            <div className='Simple'>How important is job stability or job security to you?</div>
             <div className='Simple'>Question 15</div>
+            <div className='Simple'>Do you prefer hands-on learning, reading/writing, or listening/watching?</div>
             <div className='Simple'>Question 16</div>
+            <div className='Simple'>Do you enjoy science and experiments?</div>
             <div className='Simple'>Question 17</div>
+            <div className='Simple'>Do you like debating, discussing, or presenting ideas?</div>
             <div className='Simple'>Question 18</div>
+            <div className='Simple'>Do you like analyzing patterns or coming up with new ideas?</div>
             <div className='Simple'>Question 19</div>
+            <div className='Simple'>Are you interested in going to college, trade school, or jumping right into work?</div>
             <div className='Simple'>Question 20</div>
+            <div className='Simple'>Do you prefer a fast-paced environment or a calm, steady one?</div>
             <div className='Simple'>Question 21</div>
+            <div className='Simple'>Would you like to travel for work or stay in one place?</div>
             <div className='Simple'>Question 22</div>
+            <div className='Simple'>Do you want to work for a big company, a small business, or for yourself?</div>
             <div className='Simple'>Question 23</div>
+            <div className='Simple'>Do you prefer working with technology or avoiding screens?</div>
             <div className='Simple'>Question 24</div>
+            <div className='Simple'>Would you enjoy wearing a uniform or dressing casually at work?</div>
             <div className='Simple'>Question 25</div>
+            <div className='Simple'>Are you comfortable speaking in front of groups?</div>
             <div className='Simple'>Question 26</div>
+            <div className='Simple'>Do you enjoy writing or journaling your thoughts?</div>
             <div className='Simple'>Question 27</div>
+            <div className='Simple'>Do people often come to you for advice or support?</div>
             <div className='Simple'>Question 28</div>
+            <div className='Simple'>Do you like giving instructions or following them?</div>
             <div className='Simple'>Question 29</div>
-            <div className='Simple'>Question 30</div>
+            <div className='Simple'>Are you comfortable making quick decisions?</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{/*used AI to write out the rest of the framework for the questions & question number once I wrote a few (so I don't have to write out every framework which is the same)*/}
 
     </div>
     );
